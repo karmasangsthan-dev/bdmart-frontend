5b12292712bed598c77a17b7ad5e8820f6a5fdaa
Abu Sani Faysal <tfoysalahmed@gmail.com>
2023-05-04 23:11:41 +0600

    add: price filter

--- features/product/productApi.js
+++ features/product/productApi.js
@@ -4,7 +4,6 @@ const productApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: ({ sort, filter }) => {
-        console.log({ sort, filter });
         const params = new URLSearchParams();
         params.append("sort", JSON.stringify(sort));
         params.append("filter", JSON.stringify(filter));
