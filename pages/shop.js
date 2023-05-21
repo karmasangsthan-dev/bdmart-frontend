8782a28b9c2de97b9d8489ba34a467cb2ff9f000
Md Altaf Hossen <web.altaf.1@gmail.com>
2023-05-21 15:15:28 +0600

    updated

--- pages/shop.js
+++ pages/shop.js
@@ -1,9 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout";
-import { Rating, Slider } from "@mui/material";
-import Link from "next/link";
-import Image from "next/image";
-import { toast } from "react-hot-toast";
 import { Button, Collapse, Container, Navbar, Nav } from "react-bootstrap";
 import ShopProduct from "../components/Product/ShopProduct";
 import ShopSideBar from "../components/Shop/ShopSideBar/ShopSideBar";
@@ -11,32 +7,38 @@ import {
   useGetAllProductsQuery,
   useGetProductsQuery,
 } from "../features/product/productApi";
-import Pagination from "../components/Shared/Pagination/Pagination";
-import Loading from "../components/Shared/Loading/Loading";
 import { useSelector } from "react-redux";
-import NavMenu from "../components/Shared/NavMenu/NavMenu";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Skeleton from "react-loading-skeleton";
 import { useRouter } from "next/router";
 
-
 const shop = () => {
   const router = useRouter();
+  const [params, setParams] = useState();
   const categoryTextParams = router.query?.category;
 
   const user = useSelector((state) => state.auth.user);
-  // const [pageNumber, setPageNumber] = useState(0);
   const [allProducts, setAllProducts] = useState([]);
   const [page, setPage] = useState(4);
   const [sort, setSort] = useState({
-    perPage: 15,
+    perPage: 4,
     pageNumber: 1,
     priceSort: 0,
   });
 
   const fetchMoreData = () => {
     setPage(page + 4);
+    setSort({
+      perPage: sort.perPage + 4,
+      pageNumber: 1,
+      priceSort: 0,
+    })
   };
+
+  useEffect(() => {
+    setParams(categoryTextParams);
+  }, [router.query?.category]);
+
   const [filter, setFilter] = useState({
     category: [],
     size: "",
@@ -46,12 +48,7 @@ const shop = () => {
 
   const { data, isLoading } = useGetAllProductsQuery({ perPage: page });
 
-  const totalProducts = data?.total || 0;
-  const loadedProducts = data?.data?.length || 0;
-  const hasMore = loadedProducts < totalProducts;
-
   const {
-
     data: products,
     isSuccess,
     isLoading: loading,
@@ -59,9 +56,16 @@ const shop = () => {
     error,
   } = useGetProductsQuery({ sort, filter });
 
+  const totalProducts = data?.total || 0;
+  const loadedProducts = data?.data?.length || 0;
+  const hasMore = loadedProducts < totalProducts;
+
+  console.log(sort, 'sort')
+  console.log(page, 'per page');
+  console.log({ data, products });
   useEffect(() => {
     if (
-      sort.perPage !== 15 ||
+      sort.perPage !== 4 ||
       sort.priceSort ||
       sort.pageNumber !== 1 ||
       filter.category.length ||
@@ -70,7 +74,7 @@ const shop = () => {
     ) {
       setAllProducts(products);
     } else if (
-      sort.perPage === 15 ||
+      sort.perPage === 4 ||
       !sort.priceSort ||
       sort.pageNumber === 1 ||
       !filter.category.length ||
@@ -97,6 +101,11 @@ const shop = () => {
     }
   }, [totalProducts]);
 
+  useEffect(() => {
+    // Reset page and loaded products when category params change
+    setPage(8);
+    setAllProducts([]);
+  }, [params]);
   return (
     <Layout title="Shop - Bangladesh Mart">
       <div className="shop page-content">
@@ -107,6 +116,7 @@ const shop = () => {
                 data={data}
                 filter={filter}
                 setFilter={setFilter}
+                params={params}
               />
             </aside>
             <div className="col-lg-10 pl-lg-5">
@@ -151,51 +161,59 @@ const shop = () => {
                 <div className="products">
                   {isLoading || loading ? (
                     <div className="all-products-container-shop">
-                      {
-                        [1, 2, 3, 4, 5, 6, 7, 8].map((elem, i) => {
-                          return (
-                            <div
-                              key={i}
-                              className="border shadow "
+                      {[1, 2, 3, 4, 5, 6, 7, 8].map((elem, i) => {
+                        return (
+                          <div
+                            key={i}
+                            className="border shadow "
+                            style={{
+                              width: "217px",
+                              minHeight: "217px",
+                              background: "#fff",
+                              height: "100%",
+                            }}
+                          >
+                            <Skeleton
+                              className="d-flex mx-auto mt-2"
                               style={{
-                                width: '217px',
-                                minHeight: '217px',
-                                background: '#fff',
-                                height: '100%',
+                                width: "92%",
+                                minHeight: "188px",
+                                maxHeight: "189px",
                               }}
-                            >
-                              <Skeleton
-                                className="d-flex mx-auto mt-2"
-                                style={{ width: '92%', minHeight: '188px', maxHeight: '189px' }}
-                              />
-                              <div className="product-details-card p-2">
-                                <div style={{ marginTop: '-14px' }} className="product-title ">
-                                  <span>
-                                    <Skeleton />
-                                  </span>
-                                </div>
-                                <div className="product-price">
-                                  <p className="mb-0 ">
-                                    <Skeleton style={{ width: '50px' }} />
-                                  </p>
-                                </div>
-                                <div className="old-price">
-                                  <del style={{ display: 'inline-block' }}>
-                                    <Skeleton style={{ width: '50px' }} />
-                                  </del>
-                                  <span className="ms-1">
-                                    <Skeleton style={{ width: '30px' }} />
-                                  </span>
-                                </div>
-                                <div className="d-flex align-items-center">
-                                  <Skeleton style={{ width: '80px' }} />
-                                  <Skeleton className="ms-1" style={{ width: '30px' }} />
-                                </div>
+                            />
+                            <div className="product-details-card p-2">
+                              <div
+                                style={{ marginTop: "-14px" }}
+                                className="product-title "
+                              >
+                                <span>
+                                  <Skeleton />
+                                </span>
+                              </div>
+                              <div className="product-price">
+                                <p className="mb-0 ">
+                                  <Skeleton style={{ width: "50px" }} />
+                                </p>
+                              </div>
+                              <div className="old-price">
+                                <del style={{ display: "inline-block" }}>
+                                  <Skeleton style={{ width: "50px" }} />
+                                </del>
+                                <span className="ms-1">
+                                  <Skeleton style={{ width: "30px" }} />
+                                </span>
+                              </div>
+                              <div className="d-flex align-items-center">
+                                <Skeleton style={{ width: "80px" }} />
+                                <Skeleton
+                                  className="ms-1"
+                                  style={{ width: "30px" }}
+                                />
                               </div>
                             </div>
-                          )
-                        })
-                      }
+                          </div>
+                        );
+                      })}
                     </div>
                   ) : (
                     <div className="shop-products">
@@ -204,7 +222,6 @@ const shop = () => {
                       ))}
                       {loadedProducts === 0 && <p>No products found.</p>}
                     </div>
-
                   )}
                 </div>
               </div>
@@ -212,57 +229,69 @@ const shop = () => {
                 dataLength={loadedProducts}
                 next={fetchMoreData}
                 hasMore={!isLoading && hasMore}
-                loader={<div className="all-products-container-shop mt-3">
-                  {
-                    [1, 2, 3, 4].map((elem, i) => {
+                loader={
+                  <div className="all-products-container-shop mt-3">
+                    {[1, 2, 3, 4].map((elem, i) => {
                       return (
                         <div
                           key={i}
                           className="border shadow shop-single-product"
                           style={{
-                            width: '216px',
-                            minHeight: '217px',
-                            background: '#fff',
-                            height: '100%',
+                            width: "216px",
+                            minHeight: "217px",
+                            background: "#fff",
+                            height: "100%",
                           }}
                         >
                           <Skeleton
                             className="d-flex mx-auto mt-2"
-                            style={{ width: '92%', minHeight: '188px', maxHeight: '189px' }}
+                            style={{
+                              width: "92%",
+                              minHeight: "188px",
+                              maxHeight: "189px",
+                            }}
                           />
                           <div className="product-details-card p-2">
-                            <div style={{ marginTop: '-14px' }} className="product-title ">
+                            <div
+                              style={{ marginTop: "-14px" }}
+                              className="product-title "
+                            >
                               <span>
                                 <Skeleton />
                               </span>
                             </div>
                             <div className="product-price">
                               <p className="mb-0 ">
-                                <Skeleton style={{ width: '50px' }} />
+                                <Skeleton style={{ width: "50px" }} />
                               </p>
                             </div>
                             <div className="old-price">
-                              <del style={{ display: 'inline-block' }}>
-                                <Skeleton style={{ width: '50px' }} />
+                              <del style={{ display: "inline-block" }}>
+                                <Skeleton style={{ width: "50px" }} />
                               </del>
                               <span className="ms-1">
-                                <Skeleton style={{ width: '30px' }} />
+                                <Skeleton style={{ width: "30px" }} />
                               </span>
                             </div>
                             <div className="d-flex align-items-center">
-                              <Skeleton style={{ width: '80px' }} />
-                              <Skeleton className="ms-1" style={{ width: '30px' }} />
+                              <Skeleton style={{ width: "80px" }} />
+                              <Skeleton
+                                className="ms-1"
+                                style={{ width: "30px" }}
+                              />
                             </div>
                           </div>
                         </div>
-                      )
-                    })
-                  }
-                </div>}
+                      );
+                    })}
+                  </div>
+                }
                 endMessage={
-                  data?.data && <p style={{ textAlign: 'center' }}>
-                    <b>End</b>
-                  </p>
+                  data?.data && (
+                    <p style={{ textAlign: "center" }}>
+                      <b>End</b>
+                    </p>
+                  )
                 }
               ></InfiniteScroll>
             </div>
