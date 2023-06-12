0ddc6a3ec841332640463953f18ca96262ead36f
Abu Sani Faysal <tfoysalahmed@gmail.com>
2023-06-12 16:29:47 +0600

    add movile bottom nav

--- components/Layout.js
+++ components/Layout.js
@@ -1,35 +1,27 @@
 import Head from "next/head";
 import Header from "./Shared/Header/Header";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { fetchUser } from "../features/auth/authSlice";
 import Script from "next/script";
-import Link from "next/link";
-import Footer from "./Shared/Footer/Footer";
-import { useParams } from "next/navigation";
+
 import { useRouter } from "next/router";
-import { useSetCartMutation } from "../features/auth/authApi";
+
 import { setCart, setCartProducts } from "../features/cart/cartSlice";
 import { useGetCartProductsMutation } from "../features/product/productApi";
-import axios from "axios";
+
+import MobileBottomNav from "./Shared/Header/MobileBottomNav";
 
 const Layout = ({ children, title = "Bangladesh Mart" }) => {
   const dispatch = useDispatch();
   const router = useRouter();
-  // const [token, setToken] = useState("");
-  const [cartArray, setCartArray] = useState([]);
-  const invalid = router.pathname === "/shop";
-  const invalidURL = "/shop" || "/profile";
+
   const [getCartProducts, { data, isLoading, isSuccess }] =
     useGetCartProductsMutation();
-  // const { user, isLoading } = useSelector((state) => state?.auth);
-  // const [setProductCart, { data, isSuccess, isError }] = useSetCartMutation({
-  //   token,
-  // });
 
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
-    // setToken(token);
+
     dispatch(fetchUser(token));
   }, [dispatch]);
 
@@ -44,38 +36,6 @@ const Layout = ({ children, title = "Bangladesh Mart" }) => {
   useEffect(() => {
     dispatch(setCartProducts(data?.data));
   }, [data?.data]);
-  // useEffect(() => {
-  //   const localCart = localStorage.getItem("cartProducts");
-  //   if (localCart && !user?.email) {
-  //     const cart = JSON.parse(localCart);
-  //     setCartArray(cart);
-  //     dispatch(setCart(cart));
-  //   }
-  // }, [user?.email, isLoading]);
-
-  // useEffect(() => {
-  //   const localCart = localStorage.getItem("cartProducts");
-  //   if (localCart && user?.email) {
-  //     const cart = JSON.parse(localCart);
-  //     setCartArray(cart);
-  //   }
-  // }, [user?.email]);
-  // useEffect(() => {
-  //   if (user?.email && cartArray?.length && token) {
-  //     const cartProducts = cartArray.map((product) => {
-  //       return { _id: product?.product?._id, quantity: product?.quantity };
-  //     });
-  //     setProductCart({
-  //       token,
-  //       cartProducts,
-  //       userId: user?._id,
-  //     });
-  //   }
-
-  //   if (isSuccess) {
-  //     localStorage.removeItem("cartProducts");
-  //   }
-  // }, [user?.email, cartArray]);
 
   return (
     <>
@@ -90,6 +50,7 @@ const Layout = ({ children, title = "Bangladesh Mart" }) => {
       <Header />
 
       <main>{children}</main>
+      <MobileBottomNav />
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ import { en } from "../locales/en";
 import { bn } from "../locales/bn";
 import axios from "axios";
 import { useUpdateVisitorMutation } from "../features/auth/authApi";
+import FixedBottomNavigation from "../components/Shared/Header/MobileBottomNav";
 
 export default function Home({ data }) {
   const { locale } = useRouter();
@@ -19,26 +20,23 @@ export default function Home({ data }) {
   const [updateVisitor, { isSuccess, isError, error }] =
     useUpdateVisitorMutation();
 
-
   const recordVisitorData = async () => {
     try {
-      const ipResponse = await axios.get('https://api.ipify.org?format=json');
+      const ipResponse = await axios.get("https://api.ipify.org?format=json");
       const ipAddress = ipResponse.data.ip; // Retrieve the IP address from the response
 
-      const geoIpResponse = await axios.get(`http://ip-api.com/json/${ipAddress}`);
+      const geoIpResponse = await axios.get(
+        `http://ip-api.com/json/${ipAddress}`
+      );
       const country = geoIpResponse.data.country;
       const data = { ipAddress, country };
 
-      await updateVisitor({ipAddress, country});
-      
-
+      await updateVisitor({ ipAddress, country });
     } catch (error) {
-      console.error('Error recording visitor data:', error);
+      console.error("Error recording visitor data:", error);
     }
   };
   // setInterval(recordVisitorData, 60000);
-
-  
 
   return (
     <Layout>
@@ -48,7 +46,6 @@ export default function Home({ data }) {
       <ShopDepartments t={t} />
       <Discount t={t} />
       <JustForYou t={t} />
-      <Footer></Footer>
     </Layout>
   );
 }
