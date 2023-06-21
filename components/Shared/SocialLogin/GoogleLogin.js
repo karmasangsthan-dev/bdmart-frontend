6eb9308153e8290c0d927eda9e249f9e527c0048
Md Altaf Hossen <web.altaf.1@gmail.com>
2023-06-21 08:29:38 +0600

    login page mobile responsive added

--- components/Shared/SocialLogin/GoogleLogin.js
+++ components/Shared/SocialLogin/GoogleLogin.js
@@ -5,6 +5,7 @@ import auth from "../../../firebase.init";
 import { useRouter } from "next/router";
 import { useDispatch } from "react-redux";
 import { googleLogin } from "../../../features/auth/authSlice";
+import { toast } from "react-hot-toast";
 
 export default function GoogleLogin() {
   const router = useRouter();
@@ -23,6 +24,7 @@ export default function GoogleLogin() {
       const providerId = "firebase";
       const profilePicture = user?.user?.photoURL;
       dispatch(googleLogin({ email, fullName, providerId, profilePicture }));
+      toast.success('Login successfully...!!')
       router.push(redirect || '/');
     }
   }, [user]);
@@ -31,7 +33,7 @@ export default function GoogleLogin() {
     <button
       onClick={handleGoogleSignIn}
       style={{ backgroundColor: "#d34836" }}
-      className="btn w-100 px-2 py-2 mt-3 text-white"
+      className="btn w-100 px-2 py-2  text-white google-login-btn"
     >
       <FaGooglePlusG className="h4 mb-0" /> Google
     </button>
