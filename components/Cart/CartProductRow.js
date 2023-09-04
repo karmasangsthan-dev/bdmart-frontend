c2ae7250961a507580f31a23342710acd7e49323
Abu Sani Faysal <97028772+SanyFaysal@users.noreply.github.com>
2023-09-04 12:11:51 +0600

    add: cart layout

--- components/Cart/CartProductRow.js
+++ components/Cart/CartProductRow.js
@@ -101,9 +101,10 @@ export default function CartProductRow({ product }) {
   useEffect(() => {
     setMatchedVariant(matchingElements);
   }, []);
+  console.log({ rowProduct });
   return (
     <tr key={product?._id}>
-      <td className="product-col ">
+      {/* <td className="product-col ">
         <div className="d-flex align-items-center">
           <img className="cart-image" src={product?.thumbnail} alt="product" />
           <p
@@ -116,10 +117,28 @@ export default function CartProductRow({ product }) {
               : product?.title}
           </p>
         </div>
-      </td>
+      </td> */}
 
       {matchedVariant?.map((variant) => (
         <tr className="">
+          <td className="product-col ">
+            <div className="d-flex align-items-center">
+              <img
+                className="cart-image"
+                src={product?.thumbnail}
+                alt="product"
+              />
+              <p
+                title={product?.title}
+                onClick={() => router.push(`/productDetails/${product._id}`)}
+                className="product-title cart-product-title mb-0 ms-3"
+              >
+                {product?.title?.length > 30
+                  ? `${product?.title.slice(0, 35)}...`
+                  : product?.title}
+              </p>
+            </div>
+          </td>
           <td className="quantity-col">
             <div
               style={{ height: '60px', width: '45px' }}
@@ -201,5 +220,98 @@ export default function CartProductRow({ product }) {
         </tr>
       ))}
     </tr>
+    // <>
+    //   {console.log(matchedVariant)}
+    //   {rowProduct?.variants?.map((varinatData) => {
+    //     return (
+    //       <tr key={product?._id}>
+    //         <td className="product-col ">
+    //           <div className="d-flex align-items-center">
+    //             <img
+    //               className="cart-image"
+    //               src={product?.thumbnail}
+    //               alt="product"
+    //             />
+    //             <p
+    //               title={product?.title}
+    //               onClick={() => router.push(`/productDetails/${product._id}`)}
+    //               className="product-title cart-product-title mb-0 ms-3"
+    //             >
+    //               {product?.title?.length > 30
+    //                 ? `${product?.title.slice(0, 35)}... -`
+    //                 : product?.title}
+    //             </p>
+    //           </div>
+    //         </td>
+    //         {/* <td className="total-col">
+    //           <p
+    //             style={{
+    //               backgroundColor: `rgba(${variant?.color.r}, ${variant?.color.g}, ${variant?.color.b}, ${variant?.color.a})`,
+    //               width: '25px',
+    //               height: '25px',
+    //             }}
+    //             className="product-select-color mt-2"
+    //           ></p>
+    //         </td> */}
+    //         <td className="quantity-col">
+    //           <div
+    //             style={{ height: '60px' }}
+    //             className="product-quantity  d-flex align-items-center"
+    //           >
+    //             <div className="qty-container">
+    //               <button
+    //                 onClick={() => handleQuantityDecrement(product)}
+    //                 className="qty-btn-minus btn-light"
+    //                 type="button"
+    //               >
+    //                 <i className="fa fa-minus"></i>
+    //               </button>
+    //               <input
+    //                 type="text"
+    //                 name="qty"
+    //                 value={varinatData?.quantity}
+    //                 className="input-qty cart-input-qty"
+    //               />
+    //               <button
+    //                 onClick={() => handleQuantityIncrement(product)}
+    //                 className="qty-btn-plus btn-light"
+    //                 type="button"
+    //               >
+    //                 <i className="fa fa-plus"></i>
+    //               </button>
+    //             </div>
+    //           </div>
+    //         </td>
+    //         <td className="cart-price-col">$10</td>
+    //         <td className="total-col">
+    //           <p
+    //             className=" mb-0  cart-product-price"
+    //             style={{ height: '60px' }}
+    //           >
+    //             {productPrice * rowProduct?.quantity} {currency}
+    //           </p>
+    //         </td>
+    //         <td className="remove-col">
+    //           <div
+    //             style={{ height: '60px' }}
+    //             className="d-flex justify-content-center align-items-center"
+    //           >
+    //             <button
+    //               style={{
+    //                 maxHeight: '30px',
+    //                 minHeight: '30px',
+    //                 width: '30px',
+    //               }}
+    //               onClick={() => handleRemove(product)}
+    //               className=" btn-remove-cart "
+    //             >
+    //               <i className="fa-solid fa-xmark delete-icon"></i>
+    //             </button>
+    //           </div>
+    //         </td>
+    //       </tr>
+    //     );
+    //   })}
+    // </>
   );
 }
