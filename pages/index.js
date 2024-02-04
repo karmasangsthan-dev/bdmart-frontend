6237a20960c4efa84071f8d53001e79cb0676eb1
Md Altaf Hossen <web.altaf.1@gmail.com>
2024-02-04 20:38:44 +0600

    updated metadata for seo

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,8 @@ import MobileBottomNav from "../components/Shared/Header/MobileBottomNav";
 import CartFlottingButton from "../components/Cart/CartFlottingButton";
 import Support from "../components/Support/Support";
 
-export default function Home({ data }) {
+
+export default function Home({ bestSelling }) {
   const { locale } = useRouter();
   const t = locale === "en" ? en : bn;
 
@@ -25,7 +26,7 @@ export default function Home({ data }) {
       <Banner />
       <CartFlottingButton></CartFlottingButton>
       <LandingImage />
-      <BestSelling t={t} />
+      <BestSelling data={bestSelling} t={t} />
       <ShopDepartments t={t} />
       <Discount t={t} />
       <JustForYou t={t} />
@@ -34,4 +35,16 @@ export default function Home({ data }) {
       <Support></Support>
     </Layout>
   );
+}
+
+
+export async function getServerSideProps() {
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_SITE_LINK}/api/v1/products/section?section=bestSelling`
+  const responseBestSelling = await fetch(url);
+  const bestSelling = await responseBestSelling.json();
+
+  // Return the data as props
+  return {
+    props: { bestSelling },
+  };
 }
